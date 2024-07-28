e22dc132fb035914f0129e7a2503ba51780eebad
bgomez30 <gomezbryan30.bg@gmail.com>
2024-07-27 21:52:59 -0700

    Modified Class Diagram to make more accurate connections. now shows attributes of each class

--- src/main/java/ClassDiagram.java
+++ src/main/java/ClassDiagram.java
@@ -5,7 +5,9 @@ import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.mxgraph.layout.mxIGraphLayout;
 import com.mxgraph.layout.mxOrganicLayout;
 import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxGeometry;
 import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.util.mxConstants;
 import com.mxgraph.view.mxGraph;
 
 import javax.swing.*;
@@ -91,25 +93,46 @@ public class ClassDiagram extends JPanel implements ActionListener {
             for (ClassOrInterfaceDeclaration classDeclaration : classDeclarations) {
                 String className = classDeclaration.getNameAsString();
                 if (!classCells.containsKey(className)) {
-                    mxCell classCell = (mxCell) graph.insertVertex(parent, null, className, 100, 100, 80, 30);
+                    // Create a composite cell for the class
+                    mxCell classCell = (mxCell) graph.insertVertex(parent, null, "", 100, 100, 150, 30 + 20 * classDeclaration.getFields().size(), "shape=rectangle;whiteSpace=wrap;fillColor=white;strokeColor=black");
+
+                    // Add the class name as a label inside the composite cell
+                    mxCell titleCell = new mxCell(className, new mxGeometry(0, 0, 150, 30), "shape=rectangle;fillColor=lightgray;strokeColor=black;align=center;verticalAlign=middle");
+                    titleCell.setVertex(true);
+                    graph.addCell(titleCell, classCell);
+
+                    // Add attributes inside the composite cell
+                    int attributeHeight = 30;
+                    for (var field : classDeclaration.getFields()) {
+                        String fieldName = field.getVariables().get(0).getNameAsString();
+                        mxCell attributeCell = new mxCell(fieldName, new mxGeometry(0, attributeHeight, 150, 20), "shape=rectangle;fillColor=white;strokeColor=black;align=left;verticalAlign=middle");
+                        attributeCell.setVertex(true);
+                        graph.addCell(attributeCell, classCell);
+                        attributeHeight += 20;
+                    }
+
                     classCells.put(className, classCell); // Store class cell
                 }
 
                 // Handle inheritance
                 for (ClassOrInterfaceType extendedType : classDeclaration.getExtendedTypes()) {
                     String parentClassName = extendedType.getNameAsString();
-                    mxCell parentClassCell = classCells.computeIfAbsent(parentClassName, key -> (mxCell) graph.insertVertex(parent, null, key, 100, 100, 80, 30));
+                    mxCell parentClassCell = classCells.computeIfAbsent(parentClassName, key -> createEmptyClassCell(key));
                     graph.insertEdge(parent, null, "extends", classCells.get(className), parentClassCell);
                 }
 
                 // Handle interface implementation
                 for (ClassOrInterfaceType implementedType : classDeclaration.getImplementedTypes()) {
                     String interfaceName = implementedType.getNameAsString();
-                    mxCell interfaceCell = classCells.computeIfAbsent(interfaceName, key -> (mxCell) graph.insertVertex(parent, null, key, 100, 100, 80, 30));
+                    mxCell interfaceCell = classCells.computeIfAbsent(interfaceName, key -> createEmptyClassCell(key));
                     graph.insertEdge(parent, null, "implements", classCells.get(className), interfaceCell);
                 }
             }
         }
+    }
+
+    private mxCell createEmptyClassCell(String name) {
+        return (mxCell) graph.insertVertex(parent, null, name, 100, 100, 150, 30, "shape=rectangle;whiteSpace=wrap;fillColor=white;strokeColor=black");
     }
 
     private void createEdges() {
