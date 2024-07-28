ae92907bbb84b5ead7c7753c439cb21404d672b3
TobyMui <tcmui@calpoly.edu>
2024-07-27 23:41:29 -0700

    Working State

--- src/main/java/JavaParserPanel.java
+++ src/main/java/JavaParserPanel.java
@@ -2,8 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class JavaParserPanel extends JPanel implements ActionListener {
+public class JavaParserPanel extends JPanel implements PropertyChangeListener {
 
     private JTextArea textArea;
     private DirectoryManager directoryManager;
@@ -18,36 +20,23 @@ public class JavaParserPanel extends JPanel implements ActionListener {
         add(buttonLocal, BorderLayout.NORTH);
         add(scrollPane, BorderLayout.CENTER);
         //behavior
-        buttonLocal.addActionListener(this);
+        buttonLocal.addActionListener(new ButtonActions());
         directoryManager = DirectoryManager.getInstance();
+        directoryManager.addPropertyChangeListener(this);
     }
+
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void propertyChange(PropertyChangeEvent evt) {
         textArea.setText("");
-        JFileChooser directoryChooser = new JFileChooser();
-        directoryChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        directoryChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("Java Files", "java"));
-        directoryChooser.setDialogTitle("Choose a Directory");
-        FileParser currentFile;
-        int result = directoryChooser.showOpenDialog(this);
-        try {
-            if (result == JFileChooser.APPROVE_OPTION) {
-                directoryManager.setDirectoryPath(directoryChooser.getSelectedFile().getAbsolutePath());
-                for(String file:directoryManager.getJavaFilesList()){
-                    currentFile = new FileParser(file);
-                    textArea.append("Name" + currentFile.getName() + "\n");
-                    textArea.append("Lines: " + currentFile.getLines() + "\n");
-                    textArea.append("LOC: " + currentFile.getLOC() + "\n");
-                    textArea.append("ELOC: " + currentFile.getELOC() + "\n");
-                    textArea.append("ILOC: " + currentFile.getILOC() + "\n");
-                    textArea.append("Cyclomatic Complexity:" + currentFile.getCyclomaticComplexity() + "\n");
-                    textArea.append("--------------------------------"+ "\n");
-                }
-            }
-        } catch (Exception ex) {
-            textArea.setText("Error: " + ex.getMessage());
+        for(FileParser file: directoryManager.getParsedFileList()) {
+            textArea.append("Name" +file.getName() + "\n");
+            textArea.append("Lines: " + file.getLines() + "\n");
+            textArea.append("LOC: " + file.getLOC() + "\n");
+            textArea.append("ELOC: " + file.getELOC() + "\n");
+            textArea.append("ILOC: " + file.getILOC() + "\n");
+            textArea.append("Cyclomatic Complexity:" + file.getCyclomaticComplexity() + "\n");
+            textArea.append("--------------------------------"+ "\n");
         }
     }
-
 }
